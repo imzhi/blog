004954b1b060a5ab54b9a089cfa3c9ed460a8342
imzhi <yxz_blue@126.com>
2016-01-18 07:30:10 +0800

    新样式开始

--- assets/js/app.js
+++
@@ -1,8 +0,0 @@
-(function (window, document) {
-    document.getElementById('toggle').addEventListener('click', function (e) {
-        document.getElementById('tuckedMenu').classList.toggle('custom-menu-tucked');
-        document.getElementById('toggle').classList.toggle('x');
-    });
-
-    console.log("Hello World!");
-})(this, this.document);
---
+++ assets/js/blog.js
@@ -0,0 +1,4 @@
+// 返回顶部
+$('#go_top').click(function() {
+    $('body').scrollTop(0);
+});
